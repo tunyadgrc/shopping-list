05d72035c15cebc45406d0ffd754da59cf128564
ts <ts.titang@gmail.com>
2023-12-14 22:17:41 +0700

    Update & Reset state

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 function displayItems() {
 	const itemsFromStorage = getItemsFromStorage();
@@ -19,6 +21,16 @@ function onAddItemSubmit(e) {
 	if (newItem === '') {
 		alert('Please add an Item');
 		return;
+	}
+
+	//Check for edit mode
+	if (isEditMode) {
+		const itemToEdit = itemList.querySelector('.edit-mode');
+
+		removeItemFromStorage(itemToEdit.textContent);
+		itemToEdit.classList.remove('edit-mode');
+		itemToEdit.remove();
+		isEditMode = false;
 	}
 
 	//Create item DOM element
@@ -82,7 +94,22 @@ function getItemsFromStorage() {
 function onClickItem(e) {
 	if (e.target.parentElement.classList.contains('remove-item')) {
 		removeItem(e.target.parentElement.parentElement);
+	} else {
+		setItemToEdit(e.target);
 	}
+}
+
+function setItemToEdit(item) {
+	isEditMode = true;
+
+	itemList
+		.querySelectorAll('li')
+		.forEach((i) => i.classList.remove('edit-mode'));
+
+	item.classList.add('edit-mode');
+	formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update item';
+	formBtn.style.backgroundColor = '#888';
+	itemInput.value = item.textContent;
 }
 
 function removeItem(item) {
@@ -133,6 +160,7 @@ function filterItems(e) {
 }
 
 function checkUI() {
+	itemInput.value = '';
 	const items = itemList.querySelectorAll('li');
 	if (items.length === 0) {
 		clearBtn.style.display = 'none';
@@ -141,6 +169,10 @@ function checkUI() {
 		clearBtn.style.display = 'block';
 		itemFilter.style.display = 'block';
 	}
+
+	formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+	formBtn.style.backgroundColor = '#333';
+	isEditMode = false;
 }
 
 //Initialize app
